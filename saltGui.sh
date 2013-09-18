cdcd67beb52f974366f93c58c9ab1995b8aa7b44
Sean Brown <sbrown@trideum.com>
2013-09-18 09:26:21 -0700

    Added section for Global Variables

---
+++ saltGui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This script will assist in the installation and configuration of the salt
+# minion.  You will be able to define your site/composition/master.  
+#
+# Author: Sean Brown
+# Date:   9-18-2013
+###########################################################################
+
+##
+# Define global variables
+##
+
+export saltGuiVersion="0.16.3-1.el5"
+
