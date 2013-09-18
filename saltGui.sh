07d4e6c2643e24fdf4ad1dd8b4114feafac36e9f
Sean Brown <seannaesmb@gmail.com>
2013-09-18 10:25:29 -0700

    Added beginning dialog box

--- saltGui.sh
+++ saltGui.sh
@@ -14,3 +14,23 @@
 export saltGuiVersion="0.16.3-1.el5"
 export saltGuiMinion=""
 export saltGuiMaster=""
+export introductionAnswer=""
+
+
+##
+# Opening dialog box.  Dialog box contains welcoming paragraph
+# and lists whether to continue or not
+#
+# @introductionAnswer equals valued input from "OK" or "CANCEL"
+#  OK == 0 
+#  CANCEL == 1
+##
+function introduction(){
+
+gdialog --title "Salt Installation GUI" --yesno "Thank you choosing to install Salt! \n Would you like to continue?" 6 25
+introductionAnswer=$?
+echo $introductionAnswer
+
+}
+
+introduction
